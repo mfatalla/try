6ff2579d8724730d82febea77302a474d06fedcc
mfatalla <80556524+mfatalla@users.noreply.github.com>
2021-07-04 14:24:32 +0800

    Add files via upload

--- trytry.py
+++ trytry.py
@@ -8,7 +8,6 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import numpy as np
 import datetime as dt
-from yahooquery import Ticker
 
 
 
@@ -504,38 +503,6 @@ elif menubar == 'Technical Indicators':
         if st.checkbox("View quotes"):
             st.subheader(f"{asset} historical data")
             st.write(data2)
-
-    ticker_input_ti = Ticker(asset)
-    history_args = {
-        "period": "1y",
-        "interval": "1d",
-        "start": dt.datetime.now() - dt.timedelta(days=365),
-        "end": None,
-    }
-    option_1 = st.selectbox("Select Period or Start / End Dates", ["Period", "Dates"], 0)
-    if option_1 == "Period":
-        history_args["period"] = st.selectbox(
-            "Select Period", options=Ticker.PERIODS, index=5  # pylint: disable=protected-access
-        )
-
-        history_args["start"] = None
-        history_args["end"] = None
-    else:
-        history_args["start"] = st.date_input("Select Start Date", value=history_args["start"])
-        history_args["end"] = st.date_input("Select End Date")
-        history_args["period"] = None
-
-    st.markdown("**THEN**")
-    history_args["interval"] = st.selectbox(
-        "Select Interval", options=Ticker.INTERVALS, index=8  # pylint: disable=protected-access
-    )
-    args_string = [str(k) + "='" + str(v) + "'" for k, v in history_args.items() if v is not None]
-    st.write("Dataframe")
-    dataframe_ti = ticker_input_ti.history(**history_args)
-    if isinstance(dataframe_ti, dict):
-        st.write(dataframe_ti)
-    else:
-        st.dataframe(dataframe_ti)
 
 
 elif menubar == 'Company Profile':
