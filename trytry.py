17d56621cb24221598c8ff1b2c0437a79f2d8ee0
mfatalla <80556524+mfatalla@users.noreply.github.com>
2021-07-04 09:03:15 +0800

    Add files via upload

--- trytry.py
+++ trytry.py
@@ -9,7 +9,6 @@ import technical
 from plotly.subplots import make_subplots
 import numpy as np
 import datetime as dt
-
 st.set_page_config(
     page_title = 'SLAPSOIL',
     page_icon = '💜',
@@ -324,17 +323,179 @@ elif menubar == 'News':
             col1.button("< ", on_click=prev_page2)
         else:
             col1.write("")  # this makes the empty column show up on mobile
-
         col2.write(f"Page {1 + st.session_state.page2} of {5}")
 
-
-
-
 elif menubar == 'Technical Indicators':
-    st.image('data//logo1.png')
 
-
-
+    tech = st.beta_container
+    tech = st.beta_container()
+    with tech:
+        def calcMovingAverage(datatech, size):
+            dftech = datatech.copy()
+            dftech['sma'] = dftech['Adj Close'].rolling(size).mean()
+            dftech['ema'] = dftech['Adj Close'].ewm(span=size, min_periods=size).mean()
+            dftech.dropna(inplace=True)
+            return dftech
+        def calc_macd(datatech):
+            dftech = datatech.copy()
+            dftech['ema12'] = dftech['Adj Close'].ewm(span=12, min_periods=12).mean()
+            dftech['ema26'] = dftech['Adj Close'].ewm(span=26, min_periods=26).mean()
+            dftech['macd'] = dftech['ema12'] - dftech['ema26']
+            dftech['signal'] = dftech['macd'].ewm(span=9, min_periods=9).mean()
+            dftech.dropna(inplace=True)
+            return dftech
+        def calcBollinger(datatech, size):
+            dftech = datatech.copy()
+            dftech["sma"] = dftech['Adj Close'].rolling(size).mean()
+            dftech["bolu"] = dftech["sma"] + 2 * dftech['Adj Close'].rolling(size).std(ddof=0)
+            dftech["bold"] = dftech["sma"] - 2 * dftech['Adj Close'].rolling(size).std(ddof=0)
+            dftech["width"] = dftech["bolu"] - dftech["bold"]
+            dftech.dropna(inplace=True)
+            return dftech
+        st.title('Technical Indicators')
+        st.subheader('Moving Average')
+        coMA1, coMA2 = st.beta_columns(2)
+        with coMA1:
+            numYearMA = st.number_input('Insert period (Year): ', min_value=1, max_value=10, value=2, key=0)
+        with coMA2:
+            windowSizeMA = st.number_input('Window Size (Day): ', min_value=5, max_value=500, value=20, key=1)
+        start = dt.datetime.today() - dt.timedelta(numYearMA * 365)
+        end = dt.datetime.today()
+        dataMA = yf.download(asset, start, end)
+        df_ma = calcMovingAverage(dataMA, windowSizeMA)
+        df_ma = df_ma.reset_index()
+        figMA = go.Figure()
+        figMA.add_trace(
+            go.Scatter(
+                x=df_ma['Date'],
+                y=df_ma['Adj Close'],
+                name="Prices Over Last " + str(numYearMA) + " Year(s)"
+            )
+        )
+        figMA.add_trace(
+            go.Scatter(
+                x=df_ma['Date'],
+                y=df_ma['sma'],
+                name="SMA" + str(windowSizeMA) + " Over Last " + str(numYearMA) + " Year(s)"
+            )
+        )
+        figMA.add_trace(
+            go.Scatter(
+                x=df_ma['Date'],
+                y=df_ma['ema'],
+                name="EMA" + str(windowSizeMA) + " Over Last " + str(numYearMA) + " Year(s)"
+            )
+        )
+        figMA.update_layout(legend=dict(
+            yanchor="top",
+            y=0.99,
+            xanchor="left",
+            x=0.01
+        ))
+        figMA.update_layout(legend_title_text='Trend')
+        figMA.update_yaxes(tickprefix="$")
+        st.plotly_chart(figMA, use_container_width=True)
+        st.subheader('Moving Average Convergence Divergence (MACD)')
+        numYearMACD = st.number_input('Insert period (Year): ', min_value=1, max_value=10, value=2, key=2)
+        startMACD = dt.datetime.today() - dt.timedelta(numYearMACD * 365)
+        endMACD = dt.datetime.today()
+        dataMACD = yf.download(asset, startMACD, endMACD)
+        df_macd = calc_macd(dataMACD)
+        df_macd = df_macd.reset_index()
+        figMACD = make_subplots(rows=2, cols=1,
+                                shared_xaxes=True,
+                                vertical_spacing=0.01)
+        figMACD.add_trace(
+            go.Scatter(
+                x=df_macd['Date'],
+                y=df_macd['Adj Close'],
+                name="Prices Over Last " + str(numYearMACD) + " Year(s)"
+            ),
+            row=1, col=1
+        )
+        figMACD.add_trace(
+            go.Scatter(
+                x=df_macd['Date'],
+                y=df_macd['ema12'],
+                name="EMA 12 Over Last " + str(numYearMACD) + " Year(s)"
+            ),
+            row=1, col=1
+        )
+        figMACD.add_trace(
+            go.Scatter(
+                x=df_macd['Date'],
+                y=df_macd['ema26'],
+                name="EMA 26 Over Last " + str(numYearMACD) + " Year(s)"
+            ),
+            row=1, col=1
+        )
+        figMACD.add_trace(
+            go.Scatter(
+                x=df_macd['Date'],
+                y=df_macd['macd'],
+                name="MACD Line"
+            ),
+            row=2, col=1
+        )
+        figMACD.add_trace(
+            go.Scatter(
+                x=df_macd['Date'],
+                y=df_macd['signal'],
+                name="Signal Line"
+            ),
+            row=2, col=1
+        )
+        figMACD.update_layout(legend=dict(
+            orientation="h",
+            yanchor="bottom",
+            y=1,
+            xanchor="left",
+            x=0
+        ))
+        figMACD.update_yaxes(tickprefix="$")
+        st.plotly_chart(figMACD, use_container_width=True)
+        st.subheader('Bollinger Band')
+        coBoll1, coBoll2 = st.beta_columns(2)
+        with coBoll1:
+            numYearBoll = st.number_input('Insert period (Year): ', min_value=1, max_value=10, value=2, key=6)
+        with coBoll2:
+            windowSizeBoll = st.number_input('Window Size (Day): ', min_value=5, max_value=500, value=20, key=7)
+        startBoll = dt.datetime.today() - dt.timedelta(numYearBoll * 365)
+        endBoll = dt.datetime.today()
+        dataBoll = yf.download(asset, startBoll, endBoll)
+        df_boll = calcBollinger(dataBoll, windowSizeBoll)
+        df_boll = df_boll.reset_index()
+        figBoll = go.Figure()
+        figBoll.add_trace(
+            go.Scatter(
+                x=df_boll['Date'],
+                y=df_boll['bolu'],
+                name="Upper Band"
+            )
+        )
+        figBoll.add_trace(
+            go.Scatter(
+                x=df_boll['Date'],
+                y=df_boll['sma'],
+                name="SMA" + str(windowSizeBoll) + " Over Last " + str(numYearBoll) + " Year(s)"
+            )
+        )
+        figBoll.add_trace(
+            go.Scatter(
+                x=df_boll['Date'],
+                y=df_boll['bold'],
+                name="Lower Band"
+            )
+        )
+        figBoll.update_layout(legend=dict(
+            orientation="h",
+            yanchor="bottom",
+            y=1,
+            xanchor="left",
+            x=0
+        ))
+        figBoll.update_yaxes(tickprefix="$")
+        st.plotly_chart(figBoll, use_container_width=True)
 elif menubar == 'Company Profile':
     st.title('Company Profile')
     st.subheader(info['longName'])
@@ -346,7 +507,6 @@ elif menubar == 'Company Profile':
     st.markdown('** Website **: ' + info['website'])
     st.markdown('** Business Summary **')
     st.info(info['longBusinessSummary'])
-
     fundInfo = {
         'Enterprise Value (USD)': info['enterpriseValue'],
         'Enterprise To Revenue Ratio': info['enterpriseToRevenue'],
@@ -364,17 +524,14 @@ elif menubar == 'Company Profile':
         'Five year Avg Dividend Yield (%)': info['fiveYearAvgDividendYield'],
         'Payout Ratio': info['payoutRatio']
     }
-
     fundDF = pd.DataFrame.from_dict(fundInfo, orient='index')
     fundDF = fundDF.rename(columns={0: 'Value'})
     st.subheader('Fundamental Info')
     st.table(fundDF)
-
     st.subheader('General Stock Info')
     st.markdown('** Market **: ' + info['market'])
     st.markdown('** Exchange **: ' + info['exchange'])
     st.markdown('** Quote Type **: ' + info['quoteType'])
-
     start = dt.datetime.today() - dt.timedelta(2 * 365)
     end = dt.datetime.today()
     df = yf.download(asset, start, end)
@@ -390,7 +547,6 @@ elif menubar == 'Company Profile':
             'xanchor': 'center',
             'yanchor': 'top'})
     st.plotly_chart(fig, use_container_width=True)
-
     marketInfo = {
         "Volume": info['volume'],
         "Average Volume": info['averageVolume'],
@@ -402,14 +558,9 @@ elif menubar == 'Company Profile':
         "Share Short": info['sharesShort'],
         'Short Ratio': info['shortRatio'],
         'Share Outstanding': info['sharesOutstanding']
-
     }
-
     marketDF = pd.DataFrame(data=marketInfo, index=[0])
     st.table(marketDF)
-
-    st.image('data//logo1.png')
-
 elif menubar == 'About':
     st.image('data//logo1.png')
 else:
