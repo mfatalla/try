230842abb096b07a5e3edaacd22ed4c304f0c928
mfatalla <80556524+mfatalla@users.noreply.github.com>
2021-07-04 09:25:00 +0800

    Add files via upload

--- trytry.py
+++ trytry.py
@@ -8,6 +8,8 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import numpy as np
 import datetime as dt
+import YahooQuery as Ticker
+
 st.set_page_config(
     page_title = 'SLAPSOIL',
     page_icon = '💜',
@@ -328,7 +330,40 @@ elif menubar == 'News':
             col1.write("")  # this makes the empty column show up on mobile
         col2.write(f"Page {1 + st.session_state.page2} of {5}")
 elif menubar == 'Technical Indicators':
-    st.image('data//logo1.png')
+    tickers = Ticker(asset)
+    history_args = {
+        "period": "1y",
+        "interval": "1d",
+        "start": dt.datetime.now() - dt.timedelta(days=365),
+        "end": None,
+    }
+    option_1 = st.selectbox("Select Period or Start / End Dates", ["Period", "Dates"], 0)
+    if option_1 == "Period":
+        history_args["period"] = st.selectbox(
+            "Select Period", options=Ticker.PERIODS, index=5  # pylint: disable=protected-access
+        )
+
+        history_args["start"] = None
+        history_args["end"] = None
+    else:
+        history_args["start"] = st.date_input("Select Start Date", value=history_args["start"])
+        history_args["end"] = st.date_input("Select End Date")
+        history_args["period"] = None
+
+    st.markdown("**THEN**")
+    history_args["interval"] = st.selectbox(
+        "Select Interval", options=Ticker.INTERVALS, index=8  # pylint: disable=protected-access
+    )
+    args_string = [str(k) + "='" + str(v) + "'" for k, v in history_args.items() if v is not None]
+    st.write("Dataframe")
+    dataframe = tickers.history(**history_args)
+
+    if isinstance(dataframe, dict):
+        st.write(dataframe)
+    else:
+
+        st.dataframe(dataframe)
+
 elif menubar == 'Company Profile':
     profile = st.beta_container()
     with profile:
